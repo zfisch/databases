2006be1a9155b018ddb0029604cae5bda984625f
zfisch <zacharyrobert@gmail.com>
2015-04-17 17:38:21 -0700

    got schema.sql to run and create a table

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,22 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
 CREATE TABLE messages (
-  /* Describe your table here.*/
+   id int unsigned NOT NULL auto_increment,
+   msgId int unsigned,
+   userId int unsigned,
+   roomId int unsigned,
+
+   primary key (id)
 );
 
-/* Create other tables and define schemas for them here! */
+-- /* Create other tables and define schemas for them here! */
 
 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+--   Execute this file from the command line by typing:
+--  *    mysql -u root < server/schema.sql
+--  *  to create the database and the tables.
 
